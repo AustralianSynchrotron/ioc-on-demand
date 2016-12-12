b3e031741096cfa5a74b9525465e6b41b5b49537
RobbieClarken <robbie.clarken@gmail.com>
2016-12-12 14:36:15 +1100

    Use react-router

--- ioc-dashboard/src/index.js
+++ ioc-dashboard/src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, browserHistory } from 'react-router'
+
+import App from './App'
+import Dashboard from './Dashboard'
+import './index.css'
+
+
+const Routes = (props) => (
+  <Router {...props}>
+    <Route path="/" component={App}/>
+    <Route path="/:ioc" component={Dashboard}/>
+  </Router>
+)
 
 ReactDOM.render(
-  <App />,
+  <Routes history={browserHistory} />,
   document.getElementById('root')
-);
+)
